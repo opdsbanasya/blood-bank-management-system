d3e9932746eb548df72e4773b5d5b0a9f62a2800
DHARM <opdsbanasya@gmail.com>
2024-09-21 20:18:18 +0530

    services

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Donor = () => {
     return (
         <div className="min-h-screen w-[80%] mx-auto py-10">
-            <h2 className="text-2xl font-semibold">Donor Login</h2>
+            <h2 className="text-2xl font-semibold">Donor</h2>
             <form className="flex flex-col w-1/2 px-20 mx-auto bg-zinc-200 pt-8 pb-10 gap-8 rounded-lg">
                 <h3 className="w-fit mx-auto text-lg underline">Login</h3>
                 <input className="px-4 py-2 text-black outline-none rounded-lg font-[300] text-lg border border-zinc-500" type="text" placeholder="Username" />
--- src/Components/ServicePage.js
+++ src/Components/ServicePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Services from './Services';
 
 const ServicePage = () => {
     return (
         <div className="min-h-screen"> 
-            ServicePage
+            <Services />
         </div>
     );
 }
